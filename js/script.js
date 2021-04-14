b6fcd13aa279a9db0ae17fb1a6ed531bda0f953b
Manuel della Gala <manuel.dellagala@gmail.com>
2021-04-14 22:36:39 +0200

    ok

--- js/script.js
+++ js/script.js
@@ -1,3 +1,36 @@
 function main() {
+    var lista = [
+        {
+            'attività':'uno',
+            'eseguita': false,
+        },
+        {
+            'attività':'due',
+            'eseguita': true,
+        }
+    ];
+
+    var list = $('.todos');
+    var newInput = $('.add-todo');
+    var template = $('#template li');
+
+    for (k in lista) {
+        console.log(lista[k]);
+        var item = template.clone();
+        item.find('.text').text(lista[k].attività);
+        list.append(item);
+        
+    }
+
+    newInput.keyup(function(e){
+        console.log(e);
+        if (e.key==='Enter')
+           var text = newInput.val().trim();
+           console.log(text);
+           item.find('.text').text(text);
+           list.append(item);
+           newInput.val('');
+    })
     
 }
+
---
+++ readme.md
@@ -0,0 +1,10 @@
+Esercizio di oggi: Todo list
+nome repo: js-todo-clone
+Descrizione
+Rifare la todo list come vista insieme a lezione:
+popolando gli elementi della lista con JavaScript
+inserendo un nuovo todo con un input testuale e gli eventi da tastiera
+Rimozione todo con click su icona
+Cliccando sul testo compare uno sbarramento a indicarne il completamento
+Ricordate l'utilizzo di .on()
+Per lo stile CSS potete creare / personalizzare quello che desiderate ricordate di dare però priorità alla parte JavaScript: fate una struttura base, scrivete la logica JavaScript e una volta che tutto funziona passate pure alla parte grafica dettagliata.
